1347c43d30f6dd1362acad57b85192f9a8cde0d3
Matei <mateibenchea2@gmail.com>
2026-04-15 23:57:50 +0300

    added lnfa2regex

--- tema2/lnfa2regex/lnfa2regex.cpp
+++ tema2/lnfa2regex/lnfa2regex.cpp
@@ -1,24 +1,72 @@
 #include <bits/stdc++.h>
 
-class regex {
+// $ e lambda, iar @ e multimea vida 
 
+class regex {
+private:
+    std::string s;
+public:
+
+    regex() : s("@") {}
+    regex(const std::string &s) : s(s) {}
+
+    regex& operator=(const regex &other) {
+        if(this==&other)
+            return *this;
+        this->s=other.s;
+        return *this;
+    }
+
+    regex operator+(const regex &other) const {
+        if(this->s=="@") 
+            return other;
+        if(other.s=="@")
+            return *this;
+        if(this->s==other.s)    
+            return *this;
+        return regex("("+this->s+")+("+other.s+")");
+    }
+
+    regex operator*(const regex &other) const {
+        if(this->s=="@" || other.s=="@") 
+            return regex();
+        if(this->s=="$")
+            return other;
+        if(other.s=="$")
+            return *this;
+        return regex("("+this->s+").("+other.s+")");
+    }
+
+    regex star() const {
+        if(this->s=="$" || this->s=="@")
+            return regex("$");
+        if(this->s.size()==1) {
+            return regex(this->s+"*");
+        }
+        return regex("("+this->s+")*");
+    }
+
+    friend std::ostream& operator<<(std::ostream &os, const regex &p) {
+        os << p.s;
+        return os;
+    }
 };
 
 class lnfa {
 private:
     std::vector<std::string> alphabet,states;
-    std::string init;
+    std::string init,last;
     std::unordered_map<std::string,bool> fi;
-    std::unordered_map<std::string,std::unordered_map<std::string,std::vector<std::string> > > g;
+    std::unordered_map<std::string,std::unordered_map<std::string,regex> > g;
 public:
     void add_letter(const std::string &let){
-        if(find(alphabet.begin(),alphabet.end(),let)!=alphabet.end())
+        if(std::find(alphabet.begin(),alphabet.end(),let)!=alphabet.end())
             return;
         alphabet.push_back(let);
     }
 
     void add_state(const std::string &state,int mask){
-        if(find(states.begin(),states.end(),state)!=states.end())
+        if(std::find(states.begin(),states.end(),state)!=states.end())
             return;
         states.push_back(state);
         if(mask&1){
@@ -26,17 +74,118 @@ public:
         }
         if(mask&2){
             fi[state]=1;
+            last=state;
         }
     }
 
     void add_edge(const std::string &x, const std::string &let, const std::string &y){
-        g[x][let].push_back(y);
+        regex aux(let);
+        if(g[x].find(y)==g[x].end()) {
+            g[x][y]=aux;
+        }
+        else {
+            g[x][y]=g[x][y]+aux;
+        }
     }
 
-    void delete_edge(const std::string &x, const std::string &muc, const std::string &y) {
-        if(g[x].find(muc)==g[x].end())
-            return ;
-        g[x].erase(muc);
+    void initial_prep() {
+        bool change_fin=0;
+        bool change_init=0;
+
+        if(fi.find(init)!=fi.end()) {
+            change_fin=change_init=1;
+        }
+
+        if(fi.size()>1) {
+            change_fin=1;
+        }
+        else {
+            for(const auto &state:states) {
+                if(fi.find(state)==fi.end())
+                    continue;
+                if(g[state].size()>0) {
+                    change_fin=1;
+                    break;
+                }
+            }
+        }
+
+        for(const auto &state:states) {
+            if(g[state].find(init)!=g[state].end()) {
+                change_init=1;
+                break;
+            }
+        }
+
+        if(change_init) {
+            states.push_back("q_true_init");
+            add_edge("q_true_init", "$", init);
+            init="q_true_init";
+        }
+
+        if(change_fin) {
+            states.push_back("q_true_final");
+            for(const auto &[state,_]:fi) {
+                add_edge(state,"$","q_true_final");
+            }
+            last="q_true_final";
+            fi.clear();
+            fi["q_true_final"]=1;
+        }
+    }
+
+    regex transform2regex() {
+        initial_prep();
+        for(const auto &state:states) {
+            if(state==init || (fi.find(state)!=fi.end() && fi[state])) {
+                continue;
+            }
+
+            std::vector<std::string> ant;
+            std::vector<std::string> nxt;
+
+            for(const auto &it:g) {
+                if(it.first!=state && it.second.find(state)!=it.second.end()) {
+                    ant.push_back(it.first);
+                }
+            }
+
+            for(const auto &it:g[state]) {
+                if(it.first==state)
+                    continue;
+
+                nxt.push_back(it.first);
+            }
+
+            regex r_kk("$");
+            if(g[state].find(state)!=g[state].end()) {
+                r_kk=g[state][state].star();
+            }
+
+            for(const auto &i:ant) {
+                for(const auto &j:nxt) {
+                    regex r_ik=g[i][state];
+                    regex r_kj=g[state][j];
+                    regex ans=r_ik*r_kk*r_kj;
+                    if(g[i].find(j)==g[i].end()) {
+                        g[i][j]=ans;
+                    }
+                    else {
+                        g[i][j]=g[i][j]+ans;
+                    }
+                }
+            }
+
+            g.erase(state);
+            for(auto &it:g) {
+                it.second.erase(state);
+            }
+        }
+
+        if(g[init].find(last)==g[init].end()) {
+            return regex();
+        }
+        return g[init][last];
     }
 }v;
 
@@ -103,6 +252,7 @@ void readLnfa()
 }
 
 int main() {
-
+    readLnfa();
+    std::cout << v.transform2regex() << "\n";
     return 0;
 }
